bb96df88e6c99d0d2c4197969b4cead7c6571f0f
chaz <nowledster@gmail.com>
2017-12-01 13:53:54 +0000

    Starting Challenge

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -10,30 +10,63 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
   end
   
-  # These are the 2 for creating
+  # These are the 2 for creating They Don't need Ids because 
+  # Because records have not been created yet
   def new
     
     @subject = Subject.new(:name => 'Default')
   end
   
   def create
+    @subject = Subject.new(subject_params)
     
+    if @subject.save
+      flash[:notice] = "Your Shit Was Saved Successfully"
+      redirect_to(subjects_path)
+    else
+      render('new')
+    end 
   end 
 
-  # These 2 are for editing updating
+  # These 2 are for editing updating they need IDs
   def edit
+    
+    @subject = Subject.find(params[:id])
   end
   
   def update
+     #find a new object with params
+     @subject = Subject.find(params[:id])
+       #We use the private method below for mass assignments
+       flash[:notice] = "Your Shit Was Updated Successfully"
+    if @subject.update_attributes(subject_params)
+      #redirect to the show page
+      redirect_to(subject_path(@subject))
+    else
+      render('edit')
+    end 
     
   end 
   
   # And Obviously these are for Destroy and Delete
   
   def delete
+    @subject = Subject.find(params[:id])
   end
   
   def destroy
     
+     @subject = Subject.find(params[:id])
+     
+     @subject.destroy
+     flash[:notice] = "The '#{@subject.name}' Was Destroyed Including Everything In It's Path"
+     redirect_to(subjects_path)
+    
+  end 
+  
+  private
+  
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end 
 end
